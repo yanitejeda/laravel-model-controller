c1dfeeb6a3e3b664affc08102c830ce79a14748c
yanitejeda <tejedasp@gmail.com>
2021-12-22 15:28:50 +0100

    controllo sela funzione invocata funziona

---
+++ app/Http/Controllers/movieController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class movieController extends Controller
+{
+    //
+
+    function index(){
+       return "vedo se funziona"; 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/movie","movieController@index")->name("movies.index");
